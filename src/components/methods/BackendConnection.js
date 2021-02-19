34e16326e32c7aa667d2fc06703da8d9c934df88
Nicholas Ochs <nick.ochs@yahoo.com>
2021-02-19 18:59:15 -0500

    Fixes export read only issue

--- src/components/methods/BackendConnection.js
+++ src/components/methods/BackendConnection.js
@@ -1,21 +1,16 @@
 import axios from 'axios';
 require('dotenv').config();
 
-const createUser = async (email, password) => {
+export const createUser = async (email, password) => {
     await axios.post(`${process.env.BACKEND_CONNECTION}/signup`, {
         email: email,
         password: password
     });
 };
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
     await axios.post(`${process.env.BACKEND_CONNECTION}/login`, {
         email: email,
         password: password
     });
-};
-
-module.exports = {
-    createUser,
-    login
 };
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+const signup = require('../components/methods/BackendConnection');
 
 const SignUp = () => {
     return (
