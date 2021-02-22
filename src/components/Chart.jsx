557554b9498b7469f0eb2e7db1a5f54605540f10
eric-owen <65836532+eric-owen@users.noreply.github.com>
2021-02-22 17:09:07 -0500

    reverse data so it populates correctly

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -5,6 +5,7 @@ import moment from 'moment'
 
 const Chart = ({ coin }) => {
 
+
     const getDays = (timeframe) => {
 
         const formattedDates = []
@@ -23,46 +24,34 @@ const Chart = ({ coin }) => {
 
         let priceArr = []
         let dateArr = []
-        let dataArr = []
-
-        for (const day of getDays(30)) {
-
-            let url = `https://api.coingecko.com/api/v3/coins/${coin.id}/history?date=${day}`
-            console.log(url)
-            let data = await axios.get(url)
+        let days = getDays(30)
+        for (let i = days.length - 1; i >= 0; i--) {
+            let url = `https://api.coingecko.com/api/v3/coins/${coin.id}/history?date=${days[i]}`
+            await axios.get(url)
                 .then(async res => {
 
-                    //priceArr.push(res.data.market_data.current_price.usd)
-                    //dateArr.push(day)
-                    // promises.push({
-                    //     day: day,
-                    //     price: res.data.market_data.current_price.usd
-                    // })
-                    let eachData = await dataArr.push({
-                        day: day,
-                        price: res.data.market_data.current_price.usd
-                    })
-                    let test = await dateArr.push(day)
-                    let test2 = await priceArr.push(res.data.market_data.current_price.usd)
+                    await dateArr.push(days[i])
+                    await priceArr.push(res.data.market_data.current_price.usd)
 
-                    await console.log(dateArr)
                     await setChartData({
-                        labels: dateArr,
+                        labels: await dateArr,
                         datasets: [
                             {
                                 label: 'Price',
-                                data: priceArr,
+                                data: await priceArr,
                                 backgroundColor: [
                                     'rgba(75, 192, 192, 0.6)'
                                 ]
                             }]
                     })
 
-                }).catch((err) => console.log(err))
+                })
+
+
+
+                .catch((err) => console.log(err))
 
         }
-
-
 
     }
 
