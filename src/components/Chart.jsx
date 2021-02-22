7a98dac2c8d453cc26997b6ad1dc4bd4a6c55b01
eric-owen <65836532+eric-owen@users.noreply.github.com>
2021-02-22 16:30:18 -0500

    fixed out of order api calls, 2 bugs remain

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -18,45 +18,51 @@ const Chart = ({ coin }) => {
 
     const [chartData, setChartData] = useState({})
 
-    const chart = () => {
+    const chart = async () => {
 
 
         let priceArr = []
         let dateArr = []
+        let dataArr = []
+
         for (const day of getDays(30)) {
-            if (coin.id) {
-                axios.get(`https://api.coingecko.com/api/v3/coins/${coin.id}/history?date=${day}`)
-                    .then(res => {
-                        if (res.data.market_data) {
-                            priceArr.push(res.data.market_data.current_price.usd)
-                            dateArr.push(day)
-                            //console.log(dateArr)
 
-                        } else {
-                            priceArr = []
-                            dateArr = []
-                        }
+            let url = `https://api.coingecko.com/api/v3/coins/${coin.id}/history?date=${day}`
+            console.log(url)
+            let data = await axios.get(url)
+                .then(async res => {
 
-                    }).then(() => {
-                        if (priceArr > 0) {
-                            setChartData({
-                                labels: dateArr,
-                                datasets: [
-                                    {
-                                        label: 'Price',
-                                        data: priceArr,
-                                        backgroundColor: [
-                                            'rgba(75, 192, 192, 0.6)'
-                                        ],
-                                        borderWidth: 1
-                                    }]
-                            })
-                        }
+                    //priceArr.push(res.data.market_data.current_price.usd)
+                    //dateArr.push(day)
+                    // promises.push({
+                    //     day: day,
+                    //     price: res.data.market_data.current_price.usd
+                    // })
+                    let eachData = await dataArr.push({
+                        day: day,
+                        price: res.data.market_data.current_price.usd
+                    })
+                    let test = await dateArr.push(day)
+                    let test2 = await priceArr.push(res.data.market_data.current_price.usd)
+
+                    await console.log(dateArr)
+                    await setChartData({
+                        labels: dateArr,
+                        datasets: [
+                            {
+                                label: 'Price',
+                                data: priceArr,
+                                backgroundColor: [
+                                    'rgba(75, 192, 192, 0.6)'
+                                ]
+                            }]
                     })
 
-            }
+                }).catch((err) => console.log(err))
 
         }
+
+
 
     }
 
@@ -71,6 +77,11 @@ const Chart = ({ coin }) => {
                     data={chartData} options={{
                         responsive: true,
                         title: { text: 'Price - Last 30 Days', display: true },
+                        elements: {
+                            line: {
+                                tension: 0
+                            }
+                        },
                         scales: {
                             yAxes: [{
                                 ticks: {
