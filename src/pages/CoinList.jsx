b5a65b68d2ec0d9a02841fe981b64d086d9ce419
eric-owen <65836532+eric-owen@users.noreply.github.com>
2021-02-26 18:01:44 -0500

    touching up the coin list page

--- src/pages/CoinList.jsx
+++ src/pages/CoinList.jsx
@@ -1,109 +1,154 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Data } from '../components/methods/DataAPIs'
 import { Link } from 'react-router-dom'
-import { Paper, Grid, Typography, CssBaseline, IconButton } from '@material-ui/core'
+import { Paper, Grid, Typography, IconButton, Box } from '@material-ui/core'
 import SmallChart from '../components/SmallChart'
 import StarBorderIcon from '@material-ui/icons/StarBorder';
-import { GlobalContext } from '../context/GlobalState'
+import StarIcon from '@material-ui/icons/Star';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+    wrapper: {
+        width: '90%',
+        margin: 'auto',
+    },
+
+    headers: {
+        margin: 'auto',
+        paddingBottom: theme.spacing(5)
+
+    },
+}))
 
 
 const CoinList = () => {
+    const classes = useStyles()
 
     const [coins, setCoins] = useState(null)
+    const [watchlistButton, setWatchlistButton] = useState(true)
 
-
+    let priceColor = null
 
     useEffect(async () => {
         const res = await Data.getCoins(`coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
         setCoins(res)
     }, [])
 
-
     return (
         <>
             {coins && (
-                <Paper style={{ width: '100%' }}>
-                    <Paper>
-                        <Grid container
-                            alignItems="center"
-                            justify="center"
-                            spacing={4}
-                        >
-                            <Grid item sm={1} />
-                            <Grid item sm={1}>
-                                <Typography variant="h6">Rank</Typography>
-                            </Grid>
-                            <Grid item sm={1}>
-                                <Typography variant="h6">Name</Typography>
-                            </Grid>
-                            <Grid item sm={1}>
-                                <Typography variant="h6"> Symbol</Typography>
-                            </Grid>
-                            <Grid item sm={1}>
-                                <Typography variant="h6">Price</Typography>
-                            </Grid>
-                            <Grid item sm={1}>
-                                <Typography variant="h6">24h</Typography>
-                            </Grid>
-                            <Grid item sm={1}>
-                                <Typography variant="h6">Total Vol</Typography>
-                            </Grid>
-                            <Grid item sm={1}>
-                                <Typography variant="h6">Mkt Cap</Typography>
-                            </Grid>
-                            <Grid item sm={2}>
-                                <Typography variant="h6">Last 24h</Typography>
-                            </Grid>
+                <Box
+                    component={Grid}
+                    container
+                    className={classes.wrapper}
+
+
+                >
+                    <Typography
+                        className={classes.headers}
+                        variant="h3"
+                    >
+                        Top Cryptocurrencies
+                    </Typography>
+                    <Box
+                        component={Grid}
+                        container
+                        className={classes.headers}
+                        boxShadow={3}
+                        spacing={0}
+                        justifyContent="space-between"
+                    >
+                        <Grid item sm={0} />
+                        <Grid item sm={0}>
+                            <Typography variant="h6">Rank</Typography>
                         </Grid>
-                    </Paper>
+                        <Grid item sm={1}>
+                            <Typography variant="h6">Name</Typography>
+                        </Grid>
+                        <Grid item sm={1}>
+                            <Typography variant="h6"> Symbol</Typography>
+                        </Grid>
+                        <Grid item sm={1}>
+                            <Typography variant="h6">Price</Typography>
+                        </Grid>
+                        <Grid item sm={1}>
+                            <Typography variant="h6">24h%Change</Typography>
+                        </Grid>
+                        <Grid item sm={1}>
+                            <Typography variant="h6">Mkt Cap</Typography>
+                        </Grid>
+                        <Grid item sm={3}>
+                            <Typography variant="h6">Last 24h</Typography>
+                        </Grid>
+                    </Box>
                     {coins.map((coin) => (
-                        <Paper key={coin.name}>
-                            <Grid container
-                                alignItems="center"
-                                justify="center"
-                                spacing={4}>
-                                <Grid item sm={1}>
-                                    <IconButton>
-                                        <StarBorderIcon
 
-                                        />
-                                    </IconButton>
-                                </Grid>
-                                <Grid item sm={1}>
+                        <Box container
+                            component={Grid}
+                            boxShadow={3}
+                            alignItems="center"
+                            justifyContent="space-between"
+
+                        >
+                            <Grid item sm={0} key={coin.name}>
+                                <IconButton onClick={() => setWatchlistButton(!watchlistButton)}>
+                                    {watchlistButton ? <StarBorderIcon /> : <StarIcon />}
+                                </IconButton>
+                            </Grid>
+
+
+                            <Grid item sm={0}>
+                                <Typography variant="body1">
                                     {coin.market_cap_rank}
-                                </Grid>
-                                <Grid item sm={1} component={Link} to={`/coins/${coin.id}`}>
+                                </Typography>
+                            </Grid>
+                            <Grid item sm={1} component={Link} to={`/coins/${coin.id}`}>
+                                <Typography variant="body1">
                                     {coin.name}
-                                </Grid>
-                                <Grid item sm={1}>
-                                    {coin.symbol.toUpperCase()}
-                                </Grid>
-                                <Grid item sm={1}>
-                                    ${coin.current_price.toFixed(2)}
-                                </Grid>
-                                <Grid item sm={1}>
-                                    {coin.price_change_24h.toFixed(2)}%
-                                </Grid>
-                                <Grid item sm={1}>
-                                    {coin.total_volume}
-                                </Grid>
-                                <Grid item sm={1}>
-                                    ${coin.market_cap}
-                                </Grid>
-                                <Grid item sm={2}>
-                                    <div style={{ width: '15rem', height: '3rem' }}>
-                                        <SmallChart coin={coin} price={coin.price_change_24h} />
-                                    </div>
-                                </Grid>
+                                </Typography>
 
                             </Grid>
-                        </Paper>
+                            <Grid item sm={1}>
+                                <Typography variant="body1">
+                                    {coin.symbol.toUpperCase()}
+                                </Typography>
+
+                            </Grid>
+                            <Grid item sm={1} className={classes.individualContainers}>
+                                <Typography variant="body1">
+                                    ${coin.current_price.toFixed(2)}
+                                </Typography>
+
+                            </Grid>
+                            <Grid item sm={1} style={{ color: `${priceColor}` }}>
+
+                                <Typography variant="body1" style={{ color: `${priceColor}` }}>
+                                    {coin.price_change_24h.toFixed(2)}%
+                                </Typography>
+
+                            </Grid>
+                            <Grid item sm={1} className={classes.individualContainers}>
+                                <Typography variant="body1">
+                                    ${coin.market_cap}
+                                </Typography>
+
+                            </Grid>
+                            <Grid item sm={3} className={classes.individualContainers} >
+                                <div style={{ height: '10vh' }}>
+                                    <SmallChart coin={coin} price={coin.price_change_24h} />
+                                </div>
+                            </Grid>
+
+                        </Box>
                     ))}
-                </Paper>
-            )}
+                </Box>
+            )
+            }
         </>
     )
 }
 
 export default CoinList
+
+
 
