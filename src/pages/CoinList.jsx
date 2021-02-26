d595f5b33cb3fd2cc4156e04532390e5f6aea594
eric-owen <65836532+eric-owen@users.noreply.github.com>
2021-02-25 20:24:55 -0500

    minor changes to button

--- src/pages/CoinList.jsx
+++ src/pages/CoinList.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { Data } from '../components/methods/DataAPIs'
 import { Link } from 'react-router-dom'
-import { Paper, Grid, Typography, CssBaseline } from '@material-ui/core'
+import { Paper, Grid, Typography, CssBaseline, IconButton } from '@material-ui/core'
 import SmallChart from '../components/SmallChart'
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import { GlobalContext } from '../context/GlobalState'
 
 
 const CoinList = () => {
 
     const [coins, setCoins] = useState(null)
+
+
 
     useEffect(async () => {
         const res = await Data.getCoins(`coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
@@ -21,11 +25,11 @@ const CoinList = () => {
                 <Paper style={{ width: '100%' }}>
                     <Paper>
                         <Grid container
-                            salignItems="center"
+                            alignItems="center"
                             justify="center"
                             spacing={4}
                         >
-
+                            <Grid item sm={1} />
                             <Grid item sm={1}>
                                 <Typography variant="h6">Rank</Typography>
                             </Grid>
@@ -39,7 +43,7 @@ const CoinList = () => {
                                 <Typography variant="h6">Price</Typography>
                             </Grid>
                             <Grid item sm={1}>
-                                <Typography variant="h6">24h %</Typography>
+                                <Typography variant="h6">24h</Typography>
                             </Grid>
                             <Grid item sm={1}>
                                 <Typography variant="h6">Total Vol</Typography>
@@ -54,30 +58,37 @@ const CoinList = () => {
                     </Paper>
                     {coins.map((coin) => (
                         <Paper key={coin.name}>
-                            <Grid container component={Link} to={`/coins/${coin.id}`}
+                            <Grid container
                                 alignItems="center"
                                 justify="center"
                                 spacing={4}>
                                 <Grid item sm={1}>
-                                    {coin.market_cap_rank}
+                                    <IconButton>
+                                        <StarBorderIcon
+
+                                        />
+                                    </IconButton>
                                 </Grid>
                                 <Grid item sm={1}>
+                                    {coin.market_cap_rank}
+                                </Grid>
+                                <Grid item sm={1} component={Link} to={`/coins/${coin.id}`}>
                                     {coin.name}
                                 </Grid>
                                 <Grid item sm={1}>
                                     {coin.symbol.toUpperCase()}
                                 </Grid>
                                 <Grid item sm={1}>
-                                    {coin.current_price.toFixed(2)}
+                                    ${coin.current_price.toFixed(2)}
                                 </Grid>
                                 <Grid item sm={1}>
-                                    {coin.price_change_24h.toFixed(2)}
+                                    {coin.price_change_24h.toFixed(2)}%
                                 </Grid>
                                 <Grid item sm={1}>
                                     {coin.total_volume}
                                 </Grid>
                                 <Grid item sm={1}>
-                                    {coin.market_cap}
+                                    ${coin.market_cap}
                                 </Grid>
                                 <Grid item sm={2}>
                                     <div style={{ width: '15rem', height: '3rem' }}>
