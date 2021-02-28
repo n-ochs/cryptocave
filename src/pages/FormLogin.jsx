5f26c4f5a16e02d51ddff8ccab8351d127fcd422
Nicholas Ochs <nick.ochs@yahoo.com>
2021-02-28 16:04:56 -0500

    Adds styling to login page

--- src/pages/FormLogin.jsx
+++ src/pages/FormLogin.jsx
@@ -1,3 +1,5 @@
+import { Avatar, Button, Grid, Paper, TextField, Typography } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { login } from '../components/methods/BackendConnection/Auth';
@@ -23,51 +25,26 @@ const FormLogin = () => {
         login(email, password);
     };
 
+    const paperStyle={padding: 60, height: '30vh', width: 500, margin: 'auto auto', border: "5px solid #05f4b7"};
+    const avatarStyle={backgroundColor: 'grey'};
+    const buttonStyle={margin: '10px 0'};
+    const theme={backgroundColor: '#12151f', height: '100vh'};
+
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h1>Please log in to your account.</h1>
-
-                {/* Email Field */}
-                <div>
-                    <label htmlFor="email">
-                        Email
-                    </label>
-                    <input 
-                        id="email"
-                        type="email" 
-                        name="email" 
-                        placeholder="Enter your email"
-                        value={values.email}
-                        onChange={handleChange}>
-                        </input>
-                </div>
-
-                {/* Password Field */}
-                <div>
-                    <label htmlFor="password">
-                        Password
-                    </label>
-                    <input 
-                        id="password"
-                        type="password" 
-                        name="password" 
-                        placeholder="Enter your password"
-                        value={values.password}
-                        onChange={handleChange}>
-                        </input>
-                </div>
-
-                <button
-                type="submit">
-                    Log In
-                </button>
-
-                <span>
-                    Don't have an account? <Link to="/signup">Signup here</Link>
-                </span>
-            </form>
-        </div>
+            <Grid style={theme} container>
+                <Paper elevation={10} style={paperStyle} spacing={0} justify='center'>
+                    <Grid align='center'>
+                        <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
+                        <Typography>Sign In</Typography>
+                    </Grid>
+                    <TextField fullWidth required label='Email' placeholder='Email' id='email' type='email' name='email' value={values.email} onChange={handleChange} />
+                    <TextField fullWidth required label='Password' placeholder='Password' id='password' type='password' name='password' value={values.password} onChange={handleChange} />
+                    <Button fullWidth type='submit' color='primary' variant='contained' onClick={handleSubmit} style={buttonStyle}>Sign In</Button>
+                    <Typography>
+                        Don't have an account? <Link to="/signup">Sign up</Link>
+                    </Typography>
+                </Paper>
+            </Grid>
     );
 };
 
